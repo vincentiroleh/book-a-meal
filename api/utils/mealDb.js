8b94888e7a65e47b0dcb1df14c751d6356de8db7
Vincent Iroleh <irolehiroleh@gmail.com>
2019-02-17 20:12:14 +0100

    chore: created meal model and a dummy meal database

---
+++ api/utils/mealDb.js
@@ -0,0 +1,27 @@
+const meals = [{
+  id: 1,
+  name: 'Fried Rice',
+  size: 'Medium',
+  price: '450',
+},
+{
+  id: 2,
+  name: 'Jollof Rice',
+  size: 'Large',
+  price: '550',
+},
+{
+  id: 3,
+  name: 'Beans',
+  size: 'Small',
+  price: '150',
+},
+{
+  id: 4,
+  name: 'EBa & Semo',
+  size: 'Medium',
+  price: '300',
+},
+];
+
+export default meals;
