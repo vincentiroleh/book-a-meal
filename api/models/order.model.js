76e469d01da436684dede21697f214bf9be1051d
Vincent Iroleh <irolehiroleh@gmail.com>
2019-02-18 02:58:58 +0100

    chore: created order model and dummy database

---
+++ api/models/order.model.js
@@ -0,0 +1,11 @@
+class Order {
+  constructor() {
+    this.id = null;
+    this.userId = null;
+    this.mealId = null;
+    this.quantity = null;
+    this.delivery_address = null;
+  }
+}
+
+export default Order;
--- api/utils/menuDb.js
+++ api/utils/menuDb.js
@@ -1,21 +1,22 @@
-const menu = [{
-  id: 1,
-  title: 'Evening Selection',
-  date: new Date(),
-  list: ['Rice and beans', 'Pizza and coke', 'Eba and Ofe'],
-},
-{
-  id: 2,
-  title: 'Wedding Selection',
-  date: new Date(),
-  list: ['Plantain and Fish', 'Coke and Pizza', 'Ofe and Garri'],
-},
-{
-  id: 3,
-  title: 'Valentines Selection',
-  date: new Date(),
-  list: ['Dode and fish', 'Rice and Salad', 'Beans and Plantain'],
-},
+const menu = [
+  {
+    id: 1,
+    title: 'Evening Selection',
+    date: new Date(),
+    list: ['Rice and beans', 'Pizza and coke', 'Eba and Ofe'],
+  },
+  {
+    id: 2,
+    title: 'Wedding Selection',
+    date: new Date(),
+    list: ['Plantain and Fish', 'Coke and Pizza', 'Ofe and Garri'],
+  },
+  {
+    id: 3,
+    title: 'Valentines Selection',
+    date: new Date(),
+    list: ['Dode and fish', 'Rice and Salad', 'Beans and Plantain'],
+  },
 ];
 
 export default menu;
