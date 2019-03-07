396ecf8c6afc1fc94b4a5581477eec9c4c452e15
Vincent Iroleh <irolehiroleh@gmail.com>
2019-03-08 00:06:16 +0100

    chore: updated our meal model to use ES6 and added some validations

--- api/models/meal.js
+++ api/models/meal.js
@@ -1,11 +1,42 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   const Meal = sequelize.define('Meal', {
-    title: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    description: DataTypes.STRING,
-    quantity: DataTypes.INTEGER,
-    userId: DataTypes.INTEGER
+    title: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the title for your meal'
+      }
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: {
+        args: false,
+        msg: 'Please enter the amount for the meal'
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter description for the meal'
+      }
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: {
+        args: false,
+        msg: 'Please input a quantity in stock for the meal'
+      }
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'User',
+        key: 'id',
+        as: 'userId'
+      }
+    }
   }, {});
   Meal.associate = function(models) {
     // associations can be defined here
