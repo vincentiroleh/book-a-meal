89d813e5288692240e436b6d56b802391a177037
Vincent Iroleh <irolehiroleh@gmail.com>
2019-03-07 23:38:58 +0100

    chore: refactored user model to use 'ES6' and added some validation.

--- api/models/user.js
+++ api/models/user.js
@@ -1,10 +1,51 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter your name'
+      }
+    },    
+    username:{
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter your username'
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter your email address'
+      },
+      unique: {
+        args: true,
+        msg: 'Email already exists'
+      },
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'Please enter a valid email address'
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: {
+        args: false,
+        msg: 'Please enter a password'
+      },
+      validate: {
+        isNotShort: (value) => {
+          if (value.length < 8) {
+            throw new Error('Password should be at least 8 characters');
+          }
+        },
+      },
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
