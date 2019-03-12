42150f7306f6a4b523069d8f95c925903e904c9d
Vincent Iroleh <irolehiroleh@gmail.com>
2019-03-12 02:07:02 +0100

    chore: created a token file.

---
+++ api/middleware/verifyToken.js
@@ -0,0 +1,13 @@
+export default (req, res, next) => {
+  const bearerHeader = req.headers.authorization;
+  if (typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.json({
+      message: 'You must be logged in to perform such operation',
+    });
+  }
+};
